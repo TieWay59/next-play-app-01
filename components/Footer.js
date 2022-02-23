df9f86b70520053054e1201932b9ee6490c091d4
TieWay59 <tieway59@foxmail.com>
2022-02-23 14:27:34 +0800

    ✨ feat: add footer

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+const ExternalLinkItems = [
+  { title: "hypertrons", url: "https://github.com/hypertrons" },
+  { title: "hypertrons", url: "https://github.com/hypertrons" },
+  { title: "hypertrons", url: "https://github.com/hypertrons" },
+  { title: "hypertrons", url: "https://github.com/hypertrons" },
+];
+
+export default Footer = () => (
+  <div>
+    <div>footer</div>
+    footer
+  </div>
+);
