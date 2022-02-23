c9c3245ee0db8fc3dcd84147508382735457bd1a
TieWay59 <tieway59@foxmail.com>
2022-02-23 16:44:47 +0800

    🦄 refactor:
    rearrange Home

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 // import Image from 'next/image'
-import { Box, Container, VStack } from "@chakra-ui/react";
+import { Box, Center, Container, VStack } from "@chakra-ui/react";
 import Logo from "../components/Logo";
 
 export default function Home() {
   return (
     <VStack>
       <Container bg="transparent" maxW="container.lg" centerContent>
-        <Box padding="40">
-          <Logo />
-        </Box>
+        <Center h="100vh">
+          <Logo maxW="lg" />
+        </Center>
       </Container>
     </VStack>
   );
