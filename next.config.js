3174eb91a831cf1111eb6871a85e40fb57177f35
TieWay59 <tieway59@foxmail.com>
2022-03-18 14:09:50 +0800

    ✨ feat: setup mdx
    按照 https://nextjs.org/docs/advanced-features/using-mdx#nextmdx 官网的教程添加 mdx 依赖
    暂无样式，需要调整网站基础结构

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== "production";
 
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+module.exports = withMDX({
+  // Append the default value with md extensions
   reactStrictMode: true,
-  assetPrefix: !debug ? '/next-play-app-01/' : '',  // match gh-page asset url
-  basePath: !debug ? '/next-play-app-01' : '',      // match gh-page link url
-}
+  assetPrefix: !debug ? "/next-play-app-01/" : "", // match gh-page asset url
+  basePath: !debug ? "/next-play-app-01" : "", // match gh-page link url
+
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
+g
